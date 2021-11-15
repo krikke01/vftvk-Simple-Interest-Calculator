9bc955bed9c1f22be55e626f85f9c192ed5ded76
krikke01 <vandyckk@icloud.com>
2021-11-15 15:03:20 +0100

    update

--- script.js
+++ script.js
@@ -25,6 +25,5 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerHTML=rateval;
     alert(rateval);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 
 }
